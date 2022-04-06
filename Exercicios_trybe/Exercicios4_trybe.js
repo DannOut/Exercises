023c66e277a4deaa5c65dd132ea28408b73f9852
DannOut <danielxdn@gmail.com>
2022-04-06 16:38:48 -0300

    exercicio 4, achando o menor valor no array

--- Exercicios_trybe/Exercicios4_trybe.js
+++ Exercicios_trybe/Exercicios4_trybe.js
@@ -1,5 +1,13 @@
+function getLowerNumber(numbers) {
+  let lowerNumber = numbers[0]; // atribui maxNumber ao primeiro valor do array
 
-
-
-
-
+  for (let index = 1; index < numbers.length; index++) {
+    // estrutura de repetição para percorrer todo o array
+    if (numbers[index] > lowerNumber) {
+      /* estrutura if/else se o numero na posição 1 (pois o valor 0 já está atribuido
+        ao lowerNumber) for menor que o lowerNumber, atribui o novo valor ao lowerNumber*/
+      lowerNumber = numbers[index];
+    }
+  }
+  return lowerNumber; // retorna o valor o lowerNumber com o menor valor atribuido
+}
