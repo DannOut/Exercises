4ec7bd72a6ed6e3705da9fae8acc12ad902b8603
DannOut <danielxdn@gmail.com>
2022-04-07 14:51:36 -0300

    atualização de pegar o menor valor do array

--- Exercicios_trybe/Exercicios4_trybe.js
+++ Exercicios_trybe/Exercicios4_trybe.js
@@ -5,11 +5,24 @@ function getLowerNumber(numbers) {
 
   for (let index = 1; index < numbers.length; index++) {
     // estrutura de repetição para percorrer todo o array
-    if (numbers[index] > lowerNumber) {
+    if (numbers[index] < lowerNumber) {
       /* estrutura if/else se o numero na posição 1 (pois o valor 0 já está atribuido
         ao lowerNumber) for menor que o lowerNumber, atribui o novo valor ao lowerNumber*/
       lowerNumber = numbers[index];
     }
   }
   return lowerNumber; // retorna o valor o lowerNumber com o menor valor atribuido
+}
+
+// Reaplicando o conceito no código abaixo
+
+function pegarMenorNumero(numero) {
+  let pegarMenorNumero = numero[0];
+  for (let row = 1; row < numero.length; row++) {
+    if (numero[row] < pegarMenorNumero) {
+      pegarMenorNumero = numero[row];
+    }
+  }
+
+  return pegarMenorNumero;
 }
