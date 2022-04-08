a974bf0c30325e8c3168cfbcba154e9af9ee7240
DannOut <danielxdn@gmail.com>
2022-04-08 14:11:44 -0300

    Verificar e salvar valores divisiveis por 5

---
+++ Exercicios_trybe/Exercicios11_trybe.js
@@ -0,0 +1,15 @@
+let array = [3, 5, 60, 13, 15, 24, 25];
+
+function divisible5() {
+  let resultado = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 5 == 0) {
+      resultado.push = (array[index]);
+      console.log(`O numero ${array[index]} é divisivel por 5 !`);
+      console.log ("O Valor",array[index], "Foi adicionado ao Array \n");  
+    } else console.log(`O numero ${array[index]} não é divisivel por 5 !\n`);
+  }
+  
+  return divisible5;
+}
+console.log(divisible5());
