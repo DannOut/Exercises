0ca671c91a4b908a7aea14ec8c1eb6e9e0adf0f8
DannOut <danielxdn@gmail.com>
2022-04-08 13:45:01 -0300

    verificador de maioridade

---
+++ Exercicios_trybe/Exercicios10_trybe.js
@@ -0,0 +1,18 @@
+let idade = [32, 5, 8, 10, 15, 18, 22, 30, 40, 15, 10, "jailson"];
+
+function verificadorMaioridade() {
+  for (let index = 0; index < idade.length; index++) {
+    if (idade[index] >= 18) {
+      let maior = [];
+      maior.push = idade[index];
+      console.log("MAIORIDADE", maior);
+    } else if (idade[index] < 18) {
+      let menor = [];
+      menor.push = idade[index];
+      console.log("MENORIDADE", menor);
+    } else console.log("IDADE INCORRETA");
+  }
+
+  return verificadorMaioridade;
+}
+console.log(verificadorMaioridade());
