d988a34d430be54a002dee8e200b1f4007e05652
DannOut <danielxdn@gmail.com>
2022-04-08 11:41:53 -0300

    atualização exercicio 4, removendo array e realizando teste

--- Exercicios_trybe/Exercicios4_trybe.js
+++ Exercicios_trybe/Exercicios4_trybe.js
@@ -1,28 +1,14 @@
 //Exercício 4
+let numbers = [4, 3, 1, 5, 6, 7, 34];
 
-function getLowerNumber(numbers) {
-  let lowerNumber = numbers[0]; // atribui maxNumber ao primeiro valor do array
+let lowerNumber = numbers[0]; // atribui lowerNumber ao primeiro valor do array
 
-  for (let index = 1; index < numbers.length; index++) {
-    // estrutura de repetição para percorrer todo o array
-    if (numbers[index] < lowerNumber) {
-      /* estrutura if/else se o numero na posição 1 (pois o valor 0 já está atribuido
+for (let index = 1; index < numbers.length; index++) {
+  // estrutura de repetição para percorrer todo o array
+  if (numbers[index] < lowerNumber) {
+    /* estrutura if/else se o numero na posição 1 (pois o valor 0 já está atribuido
         ao lowerNumber) for menor que o lowerNumber, atribui o novo valor ao lowerNumber*/
-      lowerNumber = numbers[index];
-    }
+    lowerNumber = numbers[index];
   }
-  return lowerNumber; // retorna o valor o lowerNumber com o menor valor atribuido
 }
-
-// Reaplicando o conceito no código abaixo
-
-function pegarMenorNumero(numero) {
-  let pegarMenorNumero = numero[0];
-  for (let row = 1; row < numero.length; row++) {
-    if (numero[row] < pegarMenorNumero) {
-      pegarMenorNumero = numero[row];
-    }
-  }
-
-  return pegarMenorNumero;
-}
+console.log(lowerNumber);
