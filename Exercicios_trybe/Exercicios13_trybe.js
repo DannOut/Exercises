685e1cecd4112501b641083b77a5818b1ad0f1ea
DannOut <danielxdn@gmail.com>
2022-04-08 16:58:18 -0300

    Semaforo Function

---
+++ Exercicios_trybe/Exercicios13_trybe.js
@@ -0,0 +1,18 @@
+//exercicio 13
+
+function semaforo(cor){
+
+    if (cor == "verde"){
+        console.log ("PROSSIGA");
+        
+    } else if (cor == "amarelo"){
+        console.log ("ATENÇÃO!");
+    
+    }else if (cor == "vermelho"){
+        console.log ("PARE!")
+    
+    }else
+return semaforo
+}
+
+console.log (semaforo("verde"));
