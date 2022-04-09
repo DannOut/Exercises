d52b9178434a766fca7f5c33469c1ffd4daa9b2a
DannOut <danielxdn@gmail.com>
2022-04-08 21:30:38 -0300

    Listagem ordem decrescente

---
+++ Exercicios_trybe/Exercicios17_trybe.js
@@ -0,0 +1,9 @@
+//Exercicios 17
+
+function decrescente(numero) {
+  for (var index = numero; index >= 1; index -= 1) {
+    console.log(index);
+  }
+  return decrescente;
+}
+console.log (decrescente(10));
