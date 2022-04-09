f7754daa32c908331fc2df1915f31a9148880cf0
DannOut <danielxdn@gmail.com>
2022-04-08 21:30:16 -0300

    Listagem ordem crescente

---
+++ Exercicios_trybe/Exercicios16_trybe.js
@@ -0,0 +1,11 @@
+//exercicios 16
+
+function crescente(numero) {
+  for (let index = 1; index <= numero; index += 1) {
+    console.log(index);
+  }
+
+  return crescente;
+}
+
+console.log(crescente(10));
