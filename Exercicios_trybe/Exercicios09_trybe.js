2f7ce63671ee548ddc2f5c9868cc98f56174c90a
DannOut <danielxdn@gmail.com>
2022-04-10 10:29:49 -0300

    Procura o maior numero do array e retorna ele v2.0

--- Exercicios_trybe/Exercicios09_trybe.js
+++ Exercicios_trybe/Exercicios09_trybe.js
@@ -2,7 +2,7 @@
 let teste = [4,5,15,3,2,7,18,8,12,13]
 
 
-// falta criar a função para fazer funcionar
+
 function maxNumber (){
 var numero = teste[0];
 
@@ -12,14 +12,15 @@ var numero = teste[0];
       numero = teste[index];
     }
   }
+  //forma de printar
  console.log (`o maior numero do array é, ${numero} !`); // uma das formas de return com texto dentro da function
 
  return numero;
 }
+//outra forma de printar
+//console.log (`o maior numero do array é: ${maxNumber()} !`); // retornando o texto + o resultado fora da function
 
-console.log (`o maior numero do array é: ${maxNumber()} !`); // retornando o texto + o resultado fora da function
-
-
+//mais uma forma de printar
 console.log (maxNumber()); // retornando todo o console.log dentro da function + o return do resultado, no caso, numero
 
 
