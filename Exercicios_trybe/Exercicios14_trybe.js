0b39ebdbb26184c1bd7b562c247f5bae5d39edc1
DannOut <danielxdn@gmail.com>
2022-04-10 10:31:29 -0300

    Verifica se o numero é positivo ou negativo v2.0

--- Exercicios_trybe/Exercicios14_trybe.js
+++ Exercicios_trybe/Exercicios14_trybe.js
@@ -6,11 +6,11 @@ function posNeg() {
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] < 0) {
       let resultado = [];
-      resultado.push = array[index];
+      resultado[index] = array[index];
       console.log(`O numero ${array[index]} é negativo`);
     } else if (array[index] >= 0) {
       let resultado = [];
-      resultado.push = array[index];
+      resultado[index] = array[index];
       console.log(`O numero ${array[index]} é positivo`);
     }
   }
