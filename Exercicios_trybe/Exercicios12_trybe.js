8edb6bc6b68daf98ac61a3111ccba85a4678fb36
DannOut <danielxdn@gmail.com>
2022-05-05 10:35:27 -0300

    Palavra de trás para frente

--- Exercicios_trybe/Exercicios12_trybe.js
+++ Exercicios_trybe/Exercicios12_trybe.js
@@ -1,13 +1,12 @@
 //Exericicio 12
 
-
-function trasPraFrente (frase){
- let palavra = ""; // atribuindo uma string texto á variavel palavra
- for (var index = frase.length - 1; index >= 0; index --){ /* enquanto index que é frase.lenght - 1 (para pegar ultimo valor do array)
- for menor que 0 (primeiro valor do array), faça decremento de 1 até chegar a 0  */ 
- palavra += frase[index]; // palavra vai fazer um incremento da frase na posição index, que é o ultimo valor do array
- }
- return palavra; //retorna a palavra de trás pra frente na variavel palavra
-        
+function trasPraFrente(frase) {
+  let palavra = ""; // atribuindo uma string texto á variavel palavra
+  for (var index = frase.length - 1; index >= 0; index--) {
+    /* enquanto index que é frase.lenght - 1 (para pegar ultimo valor do array)
+                for menor que 0 (primeiro valor do array), faça decremento de 1 até chegar a 0  */
+    palavra += frase[index]; // palavra vai fazer um incremento da frase na posição index, que é o ultimo valor do array
+  }
+  return palavra; //r            etorna a palavra de trás pra frente na variavel palavra
 }
-console.log (trasPraFrente("daniel")); // printa a variavel palavra
+console.log(trasPraFrente("daniel")); // printa a variavel palavra
